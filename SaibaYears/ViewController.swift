233931541ee68c6f82464e1e64a11b92e97c6fa3
nxtgen <nxtgen@nxtgen.local>
2017-07-25 14:24:28 -0500

    Initial

--- SaibaYears/ViewController.swift
+++ SaibaYears/ViewController.swift
@@ -9,17 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textFieldAge: UITextField!
+    
+    
+    @IBOutlet weak var textFieldZebra: UILabel!
+    @IBAction func submitPressed( sender: Any) {
+        // Test that everything works, remove once successful
+    
+        if let tempAge = textFieldAge.text{
+        
+        let textFieldZebra = Int (tempAge)! * 3
+        }
+    }
+        textFieldZebra = String (tempAge)
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
     override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
 }
-
